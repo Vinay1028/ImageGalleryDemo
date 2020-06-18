b13cad8353e7d35ab8e6a19ac459ad8d2c5aec40
Vinay1028 <54675330+Vinay1028@users.noreply.github.com>
2020-06-18 07:01:32 +0530

    Add files via upload

---
+++ ImageGalleryDemo/ImageGalleryDemo/DataFetcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;          //used to parse the JSON data Returned by server
+using System.Net.Http;          //used to fetch the data from server
+using System.IO;
+
+namespace ImageGalleryDemo
+{
+    class DataFetcher
+    {
+        async Task<string> GetDatafromService(string searchstring)
+        {
+            string readText = null;
+            try
+            {
+                var azure = @"https://imagefetcher20200529182038.azurewebsites.net";
+                string url = azure + @"/api/fetch_images?query=" + searchstring + "&max_count=5";
+                using(HttpClient c = new HttpClient())          //using HttpClient class to fetch the JSON data from server 
+                {
+                    readText = await c.GetStringAsync(url);     //returning the response as a string to readText
+                }
+            }
+            catch
+            {
+                readText = File.ReadAllText(@"Data/sampleData.json");
+            }
+            return readText;
+        }
+
+        public async Task<List<ImageItem>> GetImageData(string search)   //this method is for parsing the JSON data
+        {
+            string data = await GetDatafromService(search);
+            return JsonConvert.DeserializeObject<List<ImageItem>>(data);
+        }
+
+    }
+}
---
+++ ImageGalleryDemo/ImageGalleryDemo/ImageGallery.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using C1.Win.C1Tile;
+
+namespace ImageGalleryDemo
+{
+    public partial class ImageGallery : Form
+    {
+        DataFetcher datafetch = new DataFetcher();
+        List<ImageItem> imageList;
+        int checkedItems = 0;
+        public ImageGallery()
+        {
+            InitializeComponent();
+        }
+
+        private async void _search_Click(object sender, EventArgs e)          //search button event
+        {
+            statusStrip1.Visible = true;
+            imageList = await datafetch.GetImageData(_searchBox.Text);
+            AddTiles(imageList);
+            statusStrip1.Visible = false;
+        }
+
+        private void AddTiles(List<ImageItem> imageList)
+        {
+            _imageTileControl.Groups[0].Tiles.Clear();
+
+            foreach (var imageitem in imageList)
+            {
+                Tile tile = new Tile();
+                tile.HorizontalSize = 2;
+
+                _imageTileControl.Groups[0].Tiles.Add(tile);
+
+                Image img = Image.FromStream(new MemoryStream(imageitem.Base64));
+
+                Template t1 = new Template();
+                ImageElement ie = new ImageElement();
+                ie.ImageLayout = ForeImageLayout.Stretch;
+                tile.Template = t1;
+                tile.Image = img;
+            }
+        }
+
+        private void OnTileChecked(Object sender, TileEventArgs e)
+        {
+            checkedItems++;
+            _exportImage.Visible = true;
+        }
+
+        private void OnTileUnchecked(Object sender, TileEventArgs e )
+        {
+            checkedItems--;
+            _exportImage.Visible = checkedItems > 0;
+        }
+
+        private void _exportImage_Click(object sender, EventArgs e)
+        {
+            List<Image> images = new List<Image>();
+            foreach(Tile tile in _imageTileControl.Groups[0].Tiles)
+            {
+                if(tile.Checked)
+                {
+                    images.Add(tile.Image);
+                }
+            }
+            ConvertToPdf(images);
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.DefaultExt = "pdf";
+            saveFile.Filter = "PDF files (*.pdf)|.pdf*";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                imagePdfDocument.Save(saveFile.FileName);
+            }
+        }
+
+        private void ConvertToPdf(List<Image> images)
+        {
+            RectangleF rect = imagePdfDocument.PageRectangle;
+            bool firstPage = true;
+            foreach(var selectimg in images)
+            {
+                if(!firstPage)
+                {
+                    imagePdfDocument.NewPage();
+                }
+                firstPage = false;
+
+                rect.Inflate(-72, -72);
+                imagePdfDocument.DrawImage(selectimg, rect);
+            }
+        }
+
+        private void _exportImage_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
